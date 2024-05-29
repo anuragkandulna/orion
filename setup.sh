28374c869ea90a4ec0e8a9b3934ff7b8fc2f00d4
Anurag Kandulna <dev.ak@outlook.in>
2024-05-30 01:09:57 +0530

    added function to check if python is already installed

--- setup.sh
+++ setup.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
 
-# Simple script to initial setup
+# Function to check if Python3 is already installed
+check_python_installed() {
+  if command -v python3 &> /dev/null
+  then
+    echo "Python 3 is already installed. Version: $(python3 --version)"
+    exit 0
 
+  elif command -v python &> /dev/null
+  then
+    echo "Python 3 is already installed. Version: $(python --version)"
+    exit 0
+
+  else
+    echo "Python 3 is not installed. Proceeding with installation..."
+
+  fi
+}
+
+
+# Simple script to initial setup
 PLATFORM="$(uname)"
 echo $PLATFORM
 
