e0f9e260611e016a41d65e87c15296078c2df1c2
Anurag Kandulna <dev.ak@outlook.in>
2024-06-08 23:13:47 +0530

    added method to create venv

--- create-env.sh
+++ create-env.sh
@@ -2,17 +2,37 @@
 
 # Function to check if python 3 is installed
 check_python_installed() {
-  if command -v python3 &> dev/null
+  if command -v python3 &> /dev/null
   then
     echo "Python 3 is already installed. Version: $(python3 --version)"
 
-  elif command -v python &> dev/null
+  elif command -v python &> /dev/null
   then
     echo "Python 3 is already installed. Version: $(python --version)"
 
   else
     echo "Python 3 is not installed. Please install Python 3."
     exit 1
-
   fi
+
+create_virtualenv() {
+  local ENV="py-env-3.12"
+
+  # Cleaning up local v-environment
+  if [ -d $ENV ]; then
+    echo "Python Virtual environment ${ENV} already exist. Cleaning up the environment..."
+    rm -rf
+    echo "Successfully cleared the old environment..."
+  fi
+
+  # Creating a new environment
+  echo "Creating new virtual environment..."
+  if command -v python3 &> /dev/null
+  then
+    python3 -m venv $ENV
+
+  else
+    python -m venv $ENV
+  fi
+
 }
